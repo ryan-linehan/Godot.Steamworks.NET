f19c214d651d29db884599d777449b656e1ccbe1
RcubDev <rlinehan2@gmail.com>
2025-11-01 10:25:51 -0700

    Give players movement

--- demo/Game/Game.cs
+++ demo/Game/Game.cs
@@ -1,10 +1,13 @@
 using Godot;
 using Godot.Steamworks.Net;
 using System;
+using System.Collections.Generic;
 public partial class Game : Node
 {
     [Export]
     public Node2D World { get; set; } = null!;
+    [Export]
+    public PackedScene PlayerScene { get; set; } = null!;
     public ulong SteamLobbyId { get; set; }
     public override void _Ready()
     {
@@ -35,9 +38,10 @@ public partial class Game : Node
             var steamErr = steamMultiplayerPeer.CreateServer(0);
             if (steamErr == Error.Ok)
             {
-                Multiplayer.MultiplayerPeer = steamMultiplayerPeer;
+                Multiplayer.MultiplayerPeer = steamMultiplayerPeer;                
                 GD.Print("Hosting via Steam P2P successful");
                 GodotSteamworks.Lobby.SetLobbyData(SteamLobbyId, "host_ready", "true");
+                AddPlayer(Multiplayer.GetUniqueId());
             }
         }
         catch (Exception ex)
@@ -58,7 +62,6 @@ public partial class Game : Node
         World.Visible = true;
         try
         {
-
             var steamMultiplayerPeer = new SteamMultiplayerPeer();
             var steamErr = steamMultiplayerPeer.CreateClient(GodotSteamworks.Lobby.GetLobbyOwner(SteamLobbyId), 0);
             if (steamErr == Error.Ok)
@@ -66,11 +69,50 @@ public partial class Game : Node
                 // Use the MultiplayerPeer property for Godot compatibility
                 Multiplayer.MultiplayerPeer = steamMultiplayerPeer;
                 GD.Print("Joined via Steam P2P successful");
+                RpcId(1, MethodName.PlayerJoined, Multiplayer.GetUniqueId());
             }
         }
         catch (Exception ex)
         {
             GD.PrintErr($"Failed to join game: {ex.Message}");
+        }
+    }
+
+    [Rpc(MultiplayerApi.RpcMode.AnyPeer)]
+    private void PlayerJoined(int peerId)
+    {
+        // Only the host needs to add players as they join since they will be added to the scene
+        // by the host automatically via MultiplayerSpawner
+        if (!Multiplayer.IsServer())
+            return;
+        GD.Print($"Player Joined with Peer ID: {peerId}");
+        AddPlayer(peerId);
+        SyncExistingPlayers();
+    }
+
+    private void AddPlayer(int peerId)
+    {
+        GD.Print("Adding player " + peerId + " to the game world");
+        var player = PlayerScene.Instantiate<Player>();
+        player.PeerId = peerId;
+        World.AddChild(player, true);
+    }
+
+    [Rpc]
+    private void SyncExistingPlayers()
+    {
+        GD.Print("Syncing Existing Players in Game");
+        var players = GetTree().GetNodesInGroup("Player");
+        HashSet<int> existingPeerIds = new HashSet<int>();
+        foreach (Player player in players)
+        {
+            existingPeerIds.Add(player.PeerId);
+        }
+        foreach (var peerId in Multiplayer.GetPeers())
+        {
+            if (existingPeerIds.Contains(peerId))
+                continue;
+            AddPlayer(peerId);
         }
     }
 }
--- demo/Game/Player.cs
+++ demo/Game/Player.cs
@@ -1,15 +1,70 @@
 using Godot;
 using System;
 
-public partial class Player : Node2D
+public partial class Player : CharacterBody2D
 {
     /// <summary>
     /// Exported so we can sync it over the network using godot's MultiplayerSyncronizer
     /// </summary>
     [Export]
     public int PeerId { get; set; } = 1;
+    Vector2 _direction = Vector2.Zero;
     public override void _Ready()
     {
         base._Ready();
+        AddToGroup("Player");
+        if (PeerId == Multiplayer.GetUniqueId())
+        {
+            AddChild(new Camera2D());
+        }
+    }
+
+    public override void _Process(double delta)
+    {
+        base._Process(delta);
+        ProcessMovement(_direction);
+    }
+
+    public override void _UnhandledInput(InputEvent inputEvent)
+    {
+        GD.Print("Unhandled Input in Player " + PeerId);
+        if (Multiplayer.GetUniqueId() != PeerId)
+            return;
+        GD.Print("Processing Input for Local Player " + PeerId);
+        _direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+        _direction = new Vector2(SnapToAxis(_direction.X), SnapToAxis(_direction.Y));
+        if (Mathf.IsZeroApprox(_direction.X) && Mathf.IsZeroApprox(_direction.Y))
+        {
+            // If the player is not moving, change to idle state
+            Velocity = Vector2.Zero;
+        }
+    }
+    /// <summary>
+    /// Snaps the value to an axis based on a threshold
+    /// </summary>
+    /// <param name="value"></param>
+    /// <param name="threshold"></param>
+    /// <returns></returns>
+    int SnapToAxis(float value, float threshold = 0.4f)
+    {
+        if (value >= threshold)
+            return 1;
+        else if (value <= -threshold)
+            return -1;
+        else
+            return 0;
+    }
+
+    /// <summary>
+    /// Processes the player movement. No network code.
+    /// </summary>
+    private void ProcessMovement(Vector2 direction)
+    {
+        _direction = direction;
+        Velocity = direction * 600;
+        GD.Print("Player " + PeerId + " moving in direction " + direction);
+        MoveAndSlide();
+        // Snap position to whole pixels to avoid subpixel movement
+        GlobalPosition = new Vector2(Mathf.Round(GlobalPosition.X), Mathf.Round(GlobalPosition.Y));
     }
 }
